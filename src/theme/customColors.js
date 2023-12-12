ae09a2097783b34c8acfb4e4a085630f7b09b3cc
mangorahayk <hhayk77777@gmail.com>
2023-12-12 21:56:44 +0400

    Header Create added button in theme

--- src/theme/customColors.js
+++ src/theme/customColors.js
@@ -5,21 +5,18 @@ export const customColors = defineStyleConfig({
       card: {
          10: "#8B8B8B",
          20: "#4A4A4A",
-         30:  "#222222",
+         30: "#212526",
          40: "#313C4E"
       },
-        brand: {
-            9: "#0A222C",
-            10: "#FFFFFF",
-            50: "#011627",
-            100: "#FF0000",
-            200: "black",
-            // ...add more color shades as needed
-          },
           primary: {
             500: "#FFA500",
             600: "#FF8C00",
             // ...add more color shades as needed
           },
+          buttons:{
+            10: '#3B3B3B',
+            20: '#F05A00'
+          },
+          
     },
   })
